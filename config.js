bde7339c39a2202cce8c2caf0e20c7c9bc4ea634
Mark Booth <mrbooth@gmail.com>
2019-06-25 16:14:38 +0100

    Change api server address

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 const dev = process.env.NODE_ENV !== 'production';
 
-export const serverAddr = dev ? 'http://localhost:3000' : 'http://10.0.0.36';
+export const serverAddr = dev ? 'http://localhost:3000' : 'http://10.0.0.35';
